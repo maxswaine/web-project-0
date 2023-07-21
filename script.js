7197e91c543b13cb2c3e3d4469f6703fafcc651c
Max Swaine <maxswainework@gmail.com>
2023-07-21 17:30:25 +0100

    Completed layout for mobile, tablet and desktop. Also implemented jjavascript on my button so that it scrolls to my projects. Just started working on a drop down menu

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 function scrollToProjects() {
-    const projectsSection = document.getElementById('projects__title');
-    if (projectsSection) {
-      // Using smooth behavior to create a smooth scrolling effect
-      projectsSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-  
-  // Add a click event listener to the button
-  const scrollToProjectsButton = document.querySelector('.intro__button');
-  if (scrollToProjectsButton) {
-    scrollToProjectsButton.addEventListener('click', scrollToProjects);
-  }
-  
+  const projectsSection = document.getElementById("projects");
+  projectsSection.scrollIntoView({ behavior: "smooth" });
+}
