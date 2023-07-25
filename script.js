aacafa72ad871c1c199828b9165658282bb03517
Max Swaine <maxswainework@gmail.com>
2023-07-25 15:24:06 +0100

    Changed photos used on the website. Saving current layout for mobile and tablet - still have more to do on laptop. Also included some javascript elements to make functional links

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ function scrollToProjects() {
   const projectsSection = document.getElementById("projects");
   projectsSection.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToAbout() {
+  const aboutSection = document.getElementById("about__start");
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const linkedinLogo = document.getElementById("linkedinLogo");
+
+  linkedinLogo.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/maxswaine", "_blank");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const githubLogo = document.getElementById("githubLogo");
+
+  githubLogo.addEventListener("click", () => {
+    window.open("https://github.com/maxswaine", "_blank");
+  });
+});
